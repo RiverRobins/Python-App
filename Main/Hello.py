33f16c65f208e601bcd33b47c184c804720691a8
RiverRobins <rivergrahamrobins@gmail.com>
2020-05-07 15:10:30 -0500

    hello world

--- Main/Hello.py
+++ Main/Hello.py
@@ -3,5 +3,14 @@ from tkinter import *
 root = Tk()
 
 
+def hello():
+    l = Label(root, text="Hello World!")
+    l.pack()
+    # b1.state = "disabled"
+
+
+b1 = Button(root, command=hello, text="Button!")
+b1.pack()
+
 
 root.mainloop()
