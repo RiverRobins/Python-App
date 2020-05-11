8aa2f34bbcfd35d13a247d14f051035af5d5a512
RiverRobins <rivergrahamrobins@gmail.com>
2020-05-11 14:53:56 -0500

    added working screen and refactored buttons to not be generated via loop

--- Main/calculator.py
+++ Main/calculator.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 prev = []
+op = ""
 
 root = Tk()
 root.title("Calculator")
@@ -8,15 +9,41 @@ root.title("Calculator")
 entry = Entry(root, width=35, borderwidth=5)
 entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
+
+def up():
+    prev.append(entry.get())
+    entry.delete(0, END)
+
+
 def number(n):
-    prev.append(n)
+    entry.insert(END, n)
+    print(n)
+
+
+def add():
+    up()
+    global op
+    op = "add"
+
+
+def do():
+    global op
+    if op == "add":
+        entry.delete(0, END)
+        entry.insert(-1, str(prev[-1] + prev[-2]))
+
 
 numbers = []
-i = 1
-while i < 10:
-    numbers.append(Button(root, text=str(i), padx=40, pady=20, command=lambda: number(i)))
-    i += 1
-numbers.append(Button(root, text="", padx=40, pady=20, command=lambda: number(0)))
+numbers.append(Button(root, text="1", padx=40, pady=20, command=lambda: number("1")))
+numbers.append(Button(root, text="2", padx=40, pady=20, command=lambda: number("2")))
+numbers.append(Button(root, text="3", padx=40, pady=20, command=lambda: number("3")))
+numbers.append(Button(root, text="4", padx=40, pady=20, command=lambda: number("4")))
+numbers.append(Button(root, text="5", padx=40, pady=20, command=lambda: number("5")))
+numbers.append(Button(root, text="6", padx=40, pady=20, command=lambda: number("6")))
+numbers.append(Button(root, text="7", padx=40, pady=20, command=lambda: number("7")))
+numbers.append(Button(root, text="8", padx=40, pady=20, command=lambda: number("8")))
+numbers.append(Button(root, text="9", padx=40, pady=20, command=lambda: number("9")))
+numbers.append(Button(root, text="0", padx=40, pady=20, command=lambda: number("0")))
 
 numbers[0].grid(row=3, column=0)
 numbers[1].grid(row=3, column=1)
@@ -28,5 +55,11 @@ numbers[6].grid(row=5, column=0)
 numbers[7].grid(row=5, column=1)
 numbers[8].grid(row=5, column=2)
 numbers[9].grid(row=6, column=1)
+
+addition = Button(root, text="+", padx=40, pady=20, command=add)
+clear = Button(root, text="clear", padx=35, pady=20, command=add)
+
+addition.grid(row=7, column=0)
+clear.grid(row=6, column=2)
 
 root.mainloop()
