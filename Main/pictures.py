461e57eb23d5f6e08d01371468bafd2ba92b693f
RiverRobins <rivergrahamrobins@gmail.com>
2020-05-19 12:46:11 -0500

    added base for scroll

--- Main/pictures.py
+++ Main/pictures.py
@@ -1,21 +1,35 @@
 from tkinter import *
 import os
+
 from PIL import ImageTk, Image
 
 imgPath = os.getcwd() + "/imgs/"
 
 root = Tk()
+c = Canvas(root)
+c.pack()
+scroll = Scrollbar(c, orient="vertical", command=c.yview)
+scroll.grid(column=1)
+
+c.config(yscrollcommand=scroll.set)
 
 images = []
 
 for n in os.listdir("imgs"):
-    if n != ".DS_Store":
-        try:
-            images.append(ImageTk.PhotoImage(Image.open(imgPath + n)))
-        finally:
-            print("file: " + n + " can't be opened.")
+    if n == ".DS_Store":
+        continue
+    # try:
+    images.append(ImageTk.PhotoImage(Image.open(imgPath + n)))
+    # finally:
+    #     print("file: " + n + " can't be opened.")
 
-l = Label(image=images[0])
-l.pack()
+mainpic = Label(c, image=images[0])
+mainpic.grid(row=0, column=0, columnspan=2)
+
+back = Button(c, text="<")
+forword = Button(c, text=">")
+
+back.grid(row=3, column=0)
+forword.grid(row=3, column=1)
 
 root.mainloop()
