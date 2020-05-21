ae4ae7275f84d6528db509e6f8d2c81336cae1c6
RiverRobins <rivergrahamrobins@gmail.com>
2020-05-21 15:19:50 -0500

    finised buttons functionality

--- Main/pictures.py
+++ Main/pictures.py
@@ -9,19 +9,20 @@ root = Tk()
 
 currentImg = 0
 
-c = Canvas(root)
-c.pack()
-scroll = Scrollbar(c, orient="vertical", command=c.yview)
-scroll.pack(side=RIGHT, fill=BOTH)
-
-c.config(yscrollcommand=scroll.set)
-
 images = []
 
 for n in os.listdir("imgs"):
     if n == ".DS_Store":
         continue
     images.append(ImageTk.PhotoImage(Image.open(imgPath + n).resize((500, 500))))
+
+c = Canvas(root)
+c.pack()
+
+scroll = Scrollbar(c, orient="vertical", command=c.yview)
+scroll.pack(side=RIGHT, fill=BOTH)
+
+c.config(yscrollcommand=scroll.set)
 
 mainpic = Label(c, image=images[0])
 mainpic.pack()
@@ -31,7 +32,15 @@ buttons.pack()
 
 
 def change_img(n):
-    return
+    global currentImg
+    global images
+    if currentImg + n < 0:
+        currentImg = len(images) - 1
+    elif currentImg + n > len(images) - 1:
+        currentImg = 0
+    else:
+        currentImg += n
+    mainpic.config(image=images[currentImg])
 
 
 back = Button(buttons, text="<", command=lambda: change_img(-1))
