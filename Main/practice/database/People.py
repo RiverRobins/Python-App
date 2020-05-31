55bbee7768d62cb31f9c212d4514ba4a274812bd
RiverRobins <rivergrahamrobins@gmail.com>
2020-05-31 12:38:45 -0500

    added database stuff, however not functional yet

--- Main/practice/database/People.py
+++ Main/practice/database/People.py
@@ -1,2 +1,8 @@
 import Creds as creds
+import mysql.connector
+
+db = mysql.connector.connect(host="localhost", user=creds.mySql_user, passwd=creds.mySql_password)
+cur = db.cursor()
+
+cur.execute("CREATE TABLE people (id BIG_INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR 255, last_name VARCHAR 255), age INTEGER, sex ENUM('M', 'F', 'N/A') DEFAULT('N/A')")
 
