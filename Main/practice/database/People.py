2c0fa38e4199f71b3c0c922e9abc215f818b886f
RiverRobins <rivergrahamrobins@gmail.com>
2020-06-01 14:53:02 -0500

    database works

--- Main/practice/database/People.py
+++ Main/practice/database/People.py
@@ -1,8 +1,14 @@
 import Creds as creds
+import mysql
 import mysql.connector
 
-db = mysql.connector.connect(host="localhost", user=creds.mySql_user, passwd=creds.mySql_password)
+db = mysql.connector.connect(host="localhost", user=creds.mySql_user, passwd=creds.mySql_password, database="people")
 cur = db.cursor()
 
-cur.execute("CREATE TABLE people (id BIG_INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR 255, last_name VARCHAR 255), age INTEGER, sex ENUM('M', 'F', 'N/A') DEFAULT('N/A')")
+cur.execute("CREATE TABLE IF NOT EXISTS customers ("
+            "id BIGINT AUTO_INCREMENT PRIMARY KEY,"
+            "first_name VARCHAR(255),"
+            "last_name VARCHAR(255),"
+            "age INT,"
+            "sex ENUM('M', 'F', 'N/A') DEFAULT 'N/A');")
 
