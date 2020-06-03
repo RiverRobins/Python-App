0fb2362684d245f293869fba2bd0912f6e8e9317
RiverRobins <rivergrahamrobins@gmail.com>
2020-06-03 14:28:14 -0500

    saving

--- Main/practice/database/People.py
+++ Main/practice/database/People.py
@@ -2,7 +2,7 @@ import Creds as creds
 import mysql.connector
 from tkinter import *
 
-db = mysql.connector.connect(host="localhost", user=creds.mySql_user, passwd=creds.mySql_password)
+db = mysql.connector.connect(host="localhost", user=creds.mySql_user, passwd=creds.mySql_password, database="people")
 cur = db.cursor()
 
 cur.execute("CREATE DATABASE IF NOT EXISTS people")
@@ -26,17 +26,26 @@ class enter_new:
     sex = None
 
     def __init__(self, n):
-        self.first = Entry(n)
-        self.last = Entry(n)
-        self.age = Entry(n)
-        self.sex = Entry(n)
+        self.first = Entry(n).pack()
+        self.last = Entry(n).pack()
+        self.age = Entry(n).pack()
+        self.sex = Entry(n).pack()
 
+
+def add():
+    global add
+    cur.execute("INSERT INTO customers(first, last, age, sex) VALUES (%s, %s, %s, %s)")
 
 mainbar = Label(root).pack()
 
 create = Label(mainbar).pack()
 createText = Label(create, text="Add").pack()
 add = enter_new(create)
+add_button = Button(create, text="Submit", command=add).pack()
 
+results = []
+cur.execute("SELECT * FROM customers")
+for i in cur.fetchall():
+    results.append(Label(root, text=str(i)).pack())
 
 root.mainloop()
