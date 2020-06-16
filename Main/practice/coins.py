cc077c07706fbdf1d6e4821c34f48e32c2baba47
RiverRobins <rivergrahamrobins@gmail.com>
2020-06-15 19:42:27 -0500

    added documentation to explain code

--- Main/practice/coins.py
+++ Main/practice/coins.py
@@ -1,27 +1,27 @@
 def coinChange(coins, amount: int) -> int:
-    coins.sort(reverse=True)
-    count = 0
-    temp = 0
-    changed = True
+    coins.sort(reverse=True)   #Ensures that array is sorted from greatest to least
+    count = 0    #The amount of coins used
+    temp = 0    #The collective value of all the coins currently being used
+    changed = True   #Determines if array was affected by the loop
     while temp < amount:
         if changed:
             for coin in coins:
-                if coin + temp <= amount:
+                if coin + temp <= amount: #If it can add the coin of the current iteration without going over the amount, it will do so and continue
                     temp += coin
                     count += 1
                     break
-                elif coin + temp > amount:
+                elif coin + temp > amount: #If adding the current coin goes over the limit, move to the next smallest coin. If every coin goes over the limit, break the loop
                     if coins.index(coin) == len(coins) - 1:
                         changed = False
                         break
                     else:
                         continue
-                if temp == amount:
+                if temp == amount: #If the value of the coins in use is the desired amount, break the loop
                     changed = False
                     break
         else:
             break
-    if temp == amount:
+    if temp == amount: #if the collective value of coins is the desired amount, return amount of coins used. Else return -1
         return count
     else:
         return -1
