ec938dfab828ed8878f3bd42c3045bef1eb52aad
RiverRobins <rivergrahamrobins@gmail.com>
2020-06-22 17:44:22 -0500

    added new sort method, struggling a bit

--- Main/practice/sort.py
+++ Main/practice/sort.py
@@ -15,17 +15,30 @@ def bubble(n):
     return n
 
 
-# def linear(n):
-#     temp = []
-#     i = 0
-#     while i < len(n):
-#         i2 = 0
-#         while i2 < len(temp):
-#             if n[i] <= temp[i2]:
-#                 temp.insert(n[i], i2)
-#             i2 += 1
-#         i += 1
-#     return temp
+def linear(n):
+    changed = True
+    while changed:
+        i = 0
+        while i < len(n) - 1:
+            if n[i] < n[0]:
+                temp = n[i]
+
+                i2 = 0
+                next = 0
+                prev = 0
+                while i2 < len(n) - 1:
+                    locals()
+                    next = n[i2 + 1]
+                    n[i2] = prev
+                    i2 += 1
+
+                n[0] = temp
+                changed = True
+                break
+            else:
+                changed = False
+            i += 1
+    return n
 
 
 nums = [10, 3, 1, 2, 4, 11, 8, 6, 7]
