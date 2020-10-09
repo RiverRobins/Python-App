85728cdfbc86e9307070b627d089e83b8f806f63
RiverRobins <rivergrahamrobins@gmail.com>
2020-10-09 12:17:21 -0500

    polished gameplay

--- Main/practice/higherlower.py
+++ Main/practice/higherlower.py
@@ -14,7 +14,7 @@ class difficulty:
 difficulties = [
     difficulty(1, 50, name="Practice 50"),
     difficulty(1, 100, name="Practice 100"),
-    difficulty(1, 50, name="Practice 500"),
+    difficulty(1, 500, name="Practice 500"),
     difficulty(1, 50, 12, name="Easy 50"),
     difficulty(1, 50, 8, name="Medium 50"),
     difficulty(1, 50, 6, name="Hard 50"),
@@ -50,12 +50,12 @@ while True:
         print("Select difficulty:")
         i = 0
         while i < len(difficulties) - 1:
-            print(str(i + 1), difficulties[i])
+            print(f"{str(i + 1)}: {difficulties[i].name}")
             i += 1
         while True:
             resp = input()
             try:
-                selected_difficulty = difficulties[int(resp)]
+                selected_difficulty = difficulties[int(resp) - 1]
                 break
             except:
                 resp_val = False
@@ -76,7 +76,7 @@ while True:
                     suff = "!"
                     if selected_difficulty.guesses != math.inf:
                         suff = "(with " + str(selected_difficulty.guesses - current_guesses) + " remaining!)"
-                    print(f"You won! You guessed the correct number({number}), in {current_guesses} guess" + suff)
+                    print(f"You won! You guessed the correct number({number}), in {current_guesses} guesses" + suff)
                     break
                 else:
                     print("Invalid guess, please make sure your guess is in the right range.")
@@ -84,6 +84,8 @@ while True:
                 print("The number is " + str(number))
             else:
                 print("Invalid input, please enter a number.")
+    elif str.strip(resp) == "2":
+        print("N/A")
 
     print("Please enter a lower number for the range")
     print("Please enter a lower number for the range")
