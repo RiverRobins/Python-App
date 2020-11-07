ff0fc8336b5ac69aaa1273be7aecef38e273be1e
RiverRobins <rivergrahamrobins@gmail.com>
2020-11-06 19:14:10 -0600

    saving

--- Main/practice/Downloader.py
+++ Main/practice/Downloader.py
@@ -1,15 +1,30 @@
+# import tkinter as Tk
+# from tk import *
 from tkinter import *
+from tkinter import messagebox
+
 from pytube import *
 
 target = ""
 
-root = Tk.tk()
 
-u_input = Tk.Entry(root)
+def getTarget():
+    global target
+    target = u_input.get()
+
+    messagebox.showinfo(text=f"{target} was entered.")
+
+
+root = Tk()
+
+u_input = Entry(root)
 u_input.pack()
-label = Tk.Label(u_input, text="Enter a URL here")
+label = Label(u_input, text="Enter a URL here")
 label.pack()
 
+enter = Button(root, text="Enter", command=getTarget)
+enter.pack()
 
+root.bind("<Return>", getTarget)
 
 root.mainloop()
